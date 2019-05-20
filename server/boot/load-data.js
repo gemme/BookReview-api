aa16e78e5f751ef073dcafdc838c056331e6329e
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2019-05-19 21:51:49 -0500

    adding storage and images

--- server/boot/load-data.js
+++ server/boot/load-data.js
@@ -5,36 +5,36 @@ module.exports = (app, cb) => {
     const {Book} = app.models;
 
     const data = [
-        { title: 'El Perfume', author:'Patrick Süskind' },
-        { title: 'La insoportable levedad del ser', author:'Milan Kundera' },
-        { title: 'Azteca', author:'Gary Jennings' },
-        { title: 'Moby Dick', author:'Herman Melville' },
-        { title: 'Lord of the Flies', author:'William Golding' },
-        { title: 'The Scarlet Letter', author:'Nathaniel Hawthorne' },
-        { title: 'The Old Man and the Sea', author:'Ernest Hemingway' },
-        { title: 'Robinson Crusoe', author:'Daniel Defoe' },
-        { title: 'Oliver Twist', author:'Charles Dickens' },
-        { title: 'Lolita', author:'Vladimir Nabokov' },
-        { title: 'Frankenstein', author:'Mary Shelley' },
-        { title: 'The Charterhouse of Parma', author:'Stendhal' },
-        { title: 'The Count of Monte Cristo', author:'Alexandre Dumas' },
-        { title: 'David Copperfield', author:'Charles Dickens' },
-        { title: 'Madame Bovary', author:'Gustave Flaubert' },
-        { title: 'Alice\'s Adventures In Wonderland', author:'Lewis Carroll' },
-        { title: 'The Woman in White', author:'Wilkie Collins' },
-        { title: 'Little Women', author:' Louisa M. Alcott' },
-        { title: 'The Way We Live Now ', author:'Anthony Trollope' },
-        { title: 'Anna Karenina', author:'Leo Tolstoy' },
-        { title: 'The Brothers Karamazov', author:'Fyodor Dostoevsky' },
-        { title: 'The Portrait of a Lady', author:'Henry James' },
-        { title: 'The Trial', author:'Franz Kafka' },
-        { title: 'Men Without Women', author:'Ernest Hemingway' },
-      ];
-
+        { title: 'El Perfume', author:'Patrick Süskind', image: 'el_perfume.jpg' },
+        { title: 'La insoportable levedad del ser', author:'Milan Kundera', image: 'la_insoportable.jpg' },
+        { title: 'Azteca', author:'Gary Jennings', image: 'azteca.jpg' },
+        { title: 'Moby Dick', author:'Herman Melville', image: 'moby-dick.jpg' },
+        { title: 'Lord of the Flies', author:'William Golding', image: 'lord_of_flies.jpg' },
+        { title: 'The Scarlet Letter', author:'Nathaniel Hawthorne', image: 'the_scarlet.jpg' },
+        { title: 'The Old Man and the Sea', author:'Ernest Hemingway', image: 'the_old_man_n_sea.jpg' },
+        { title: 'Robinson Crusoe', author:'Daniel Defoe' , image: 'robinson.jpg'},
+        { title: 'Oliver Twist', author:'Charles Dickens', image: 'oliver_twist.jpg'},
+        { title: 'Lolita', author:'Vladimir Nabokov', image: 'lolita.jpg' },
+        { title: 'Frankenstein', author:'Mary Shelley', image: 'frankenstein.jpg'},
+        { title: 'The Charterhouse of Parma', author:'Stendhal', image: 'the_chaterhouse.jpg' },
+        { title: 'The Count of Monte Cristo', author:'Alexandre Dumas', image: 'the_count_of_montecristo' },
+        { title: 'David Copperfield', author:'Charles Dickens', image: 'david_copperfield.jpg' },
+        { title: 'Madame Bovary', author:'Gustave Flaubert', image: 'madame_bovary.jpg' },
+        { title: 'Alice\'s Adventures In Wonderland', author:'Lewis Carroll', image: 'alice_adventures.jpgs' },
+        { title: 'The Woman in White', author:'Wilkie Collins', image: 'the_woman_in_white.jpg' },
+        { title: 'Little Women', author:' Louisa M. Alcott', image: 'little_women.jpg' },
+        { title: 'The Way We Live Now ', author:'Anthony Trollope', image: 'the_way_we.jpg' },
+        { title: 'Anna Karenina', author:'Leo Tolstoy', image: 'anna_karenina.jpg' },
+        { title: 'The Brothers Karamazov', author:'Fyodor Dostoevsky', image: 'karamazov.jpg' },
+        { title: 'The Portrait of a Lady', author:'Henry James', image: 'the_portrait.jpg' },
+        { title: 'The Trial', author:'Franz Kafka', image: 'the_trial.jpg' },
+        { title: 'Men Without Women', author:'Ernest Hemingway', image: 'men_without_women.jpg' }
+    ];
 
     Book.create(data,
         (err, instance, created) => {
             if(err) cb(err);
             cb(null, instance);
-      });
+       }
+    );
 }
